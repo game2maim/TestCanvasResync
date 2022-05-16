57f4f5c127a6540bcf9270137018a9bcc3e65f52
Leander Fernandes <leander.fernandes@lntinfotech.com>
2022-05-16 11:27:17 +0530

    test mave project commit

---
+++ resynctest/src/main/java/com/lti/resynctest/ResynctestApplication.java
@@ -0,0 +1,13 @@
+package com.lti.resynctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResynctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResynctestApplication.class, args);
+	}
+
+}
---
+++ resynctest/src/test/java/com/lti/resynctest/ResynctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.resynctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResynctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
